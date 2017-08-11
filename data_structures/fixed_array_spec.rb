a459910c76b538bfeaf4b8423a501d0b706ef6bf
gmmowry <glenna.mowry@devbootcamp.com>
2017-08-11 14:30:04 -0500

    Added tests for instance methods #set and #get with valid and invalid arguments

--- data_structures/fixed_array_spec.rb
+++ data_structures/fixed_array_spec.rb
@@ -3,13 +3,28 @@ require_relative 'fixed_array'
 describe 'FixedArray' do
 
   it 'instantiates a new fixed array with space for number of items passed in as parameter' do
-    array = FixedArray.new(8)
-    expect(array.size).to eq 8
+    fixed_array = FixedArray.new(8)
+    expect(fixed_array.array.length).to eq 8
   end
 
-  # it do
-  # end
+  it 'can retrieve a value in the array based on index' do
+    fixed_array = FixedArray.new(8)
+    expect(fixed_array.get(7)).to eq nil
+  end
 
-  # it do
-  # end
+  it 'can set a value in the array given an index' do
+    fixed_array = FixedArray.new(4)
+    fixed_array.set(3, "hello")
+    expect(fixed_array.get(3)).to eq "hello"
+  end
+
+  it 'throws an out of bounds error when an index given is not valid' do
+    fixed_array = FixedArray.new(4)
+    expect{fixed_array.get(7)}.to raise_error(OutOfBoundsException)
+  end
+
+  it 'throws an out of bounds error when a given index is not valid' do
+    fixed_array = FixedArray.new(4)
+    expect{fixed_array.set(7, "invalid")}.to raise_error(OutOfBoundsException)
+  end
 end
